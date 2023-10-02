91e5a29a27e27ed8343d2ba6754193c8a62e3b93
JVPortage <101283345+JVPortage@users.noreply.github.com>
2023-10-02 17:26:44 -0400

    Create fishingRegulations.js

---
+++ fishingRegulations.js
@@ -0,0 +1,159 @@
+export const fishingRegulations = [
+  {
+    species: "Black Sea Bass",
+    nicknames: ["Biscuit", "Knob Head"],
+    seasons: [
+      {
+        open: "2023-06-23",
+        close: "2023-08-31",
+        minimumSize: 15,
+        maximumSize: null,
+        bagLimit: 3
+      },
+      {
+        open: "2023-09-01",
+        close: "2023-12-31",
+        minimumSize: 15,
+        maximumSize: null,
+        bagLimit: 7
+      }
+    ]
+  },
+  {
+    species: "Bluefish",
+    nicknames: ["Snapper", "Gator", "Gorilla"],
+    seasons: [
+      {
+        open: null,
+        close: null,
+        minimumSize: null,
+        maximumSize: null,
+        bagLimit: 3
+      }
+    ]
+  },
+  {
+    species: "Cod",
+    nicknames: null,
+    seasons: [
+      {
+        open: null,
+        close: null,
+        minimumSize: 22,
+        maximumSize: null,
+        bagLimit: 10
+      }
+    ]
+  },
+  {
+    species: "Fluke",
+    nicknames: ["Summer Flounder"],
+    seasons: [
+      {
+        open: "2023-05-04",
+        close: "2023-09-30",
+        minimumSize: 19,
+        maximumSize: null,
+        bagLimit: 4
+      }
+    ]
+  },
+  {
+    species: "Haddock",
+    nicknames: null,
+    seasons: [
+      {
+        open: null,
+        close: null,
+        minimumSize: 18,
+        maximumSize: null,
+        bagLimit: null
+      }
+    ]
+  },
+  {
+    species: "Oyster Toadfish",
+    nicknames: ["Hacklehead"],
+    seasons: [
+      {
+        open: null,
+        close: null,
+        minimumSize: 10,
+        maximumSize: null,
+        bagLimit: 3
+      }
+    ]
+  },
+  {
+    species: "Pollock",
+    nicknames: null,
+    seasons: [
+      {
+        open: null,
+        close: null,
+        minimumSize: 19,
+        maximumSize: null,
+        bagLimit: null
+      }
+    ]
+  },
+  {
+    species: "Porgy",
+    nicknames: ["Scup", "Sea Bream"],
+    seasons: [
+      {
+        open: null,
+        close: null,
+        minimumSize: 9,
+        maximumSize: null,
+        bagLimit: 30
+      }
+    ]
+  },
+  {
+    species: "Striped Bass",
+    nicknames: ["Striper", "Rockfish"],
+    seasons: [
+      {
+        open: "2023-04-15",
+        close: "2023-12-15",
+        minimumSize: 28,
+        maximumSize: 31,
+        bagLimit: 1
+      }
+    ]
+  },
+  {
+    species: "Tautog",
+    nicknames: ["Blackfish"],
+    seasons: [
+      {
+        open: "2023-04-01",
+        close: "2023-04-30",
+        minimumSize: 16,
+        maximumSize: null,
+        bagLimit: 2
+      },
+      {
+        open: "2023-10-11",
+        close: "2023-12-09",
+        minimumSize: 16,
+        maximumSize: null,
+        bagLimit: 3
+      }
+    ]
+  },
+  {
+    species: "Weakfish",
+    nicknames: "Northern Sea Trout",
+    seasons: [
+      {
+        open: null,
+        close: null,
+        minimumSize: 16,
+        maximumSize: null,
+        bagLimit: 1
+      }
+    ]
+  }
+];
